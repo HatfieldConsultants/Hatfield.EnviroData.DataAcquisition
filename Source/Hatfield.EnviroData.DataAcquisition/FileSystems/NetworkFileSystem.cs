f6f2567a05fc18989ba4f461ad1dd454c2e83b36
Kyle Garsuta <kyle.garsuta@gmail.com>
2015-05-08 12:40:18 -0700

    Update NetworkFileSystem class to use NetworkDrive class Add NetworkDrive class Add Impersonation class

--- Source/Hatfield.EnviroData.DataAcquisition/FileSystems/NetworkFileSystem.cs
+++ Source/Hatfield.EnviroData.DataAcquisition/FileSystems/NetworkFileSystem.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Net;
-using System.Runtime.InteropServices;
-using System.Security.Principal;
-using System.Security.Permissions;
-using Microsoft.Win32.SafeHandles;
-using System.Runtime.ConstrainedExecution;
-using System.ComponentModel;
-using System.Security;
 
 using Hatfield.EnviroData.DataAcquisition;
 
@@ -18,20 +11,12 @@ namespace Hatfield.EnviroData.DataAcquisition.FileSystems
 {
     public class NetworkFileSystem : IFileSystem
     {
-        private string _filePath;
-        NetworkCredential _credentials;
-
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
-            int dwLogonType, int dwLogonProvider, out SafeTokenHandle phToken);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public extern static bool CloseHandle(IntPtr handle);
+        private string _filePath = null;
+        private NetworkCredential _credentials = null;
 
         public NetworkFileSystem(string filePath)
         {
             _filePath = filePath;
-            _credentials = null;
         }
 
         public NetworkFileSystem(string filePath, string username, string password, string domain)
@@ -42,59 +27,35 @@ namespace Hatfield.EnviroData.DataAcquisition.FileSystems
 
         public DataFromFileSystem FetchData()
         {
-            string fileName;
-            FileStream fileStream;
+            Stream stream;
 
-            // If user credentials are provided, impersonate the given user
             if (_credentials != null)
             {
-                SafeTokenHandle safeTokenHandle;
+                string domain = _credentials.Domain;
                 string username = _credentials.UserName;
                 string password = _credentials.Password;
-                string domain = _credentials.Domain;
+                string shareName = Path.GetPathRoot(_filePath);
 
-                if (LogonUser(username, domain, password, 2, 0, out safeTokenHandle) == true)
-                {
-                    using (WindowsIdentity newId = new WindowsIdentity(safeTokenHandle.DangerousGetHandle()))
-                    {
-                        using (WindowsImpersonationContext impersonatedUser = newId.Impersonate())
-                        {
-                            fileName = Path.GetFileName(_filePath);
-                            fileStream = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Win32Exception(Marshal.GetLastWin32Error());
-                }
+                NetworkDrive networkDrive = new NetworkDrive();
+                IntPtr impersonationHandle = networkDrive.MapDrive(domain, username, password, shareName);
+                string localDrive = networkDrive.LocalDrive;
+                string localFilePath = localDrive + @"\" + _filePath.Substring(Path.GetPathRoot(_filePath).Length);
+
+                FileStream fileStream = new FileStream(localFilePath, FileMode.Open, FileAccess.Read);
+                stream = new MemoryStream();
+                fileStream.CopyTo(stream);
+                stream.Position = 0;
+
+                networkDrive.UnMapDrive(impersonationHandle);
             }
             else
             {
-                fileName = Path.GetFileName(_filePath);
-                fileStream = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
+                stream = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
             }
 
-            return new DataFromFileSystem(fileName, fileStream);
-        }
-    }
+            string fileName = Path.GetFileName(_filePath);
 
-    public sealed class SafeTokenHandle : SafeHandleZeroOrMinusOneIsInvalid
-    {
-        private SafeTokenHandle()
-            : base(true)
-        {
-        }
-
-        [DllImport("kernel32.dll")]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-        [SuppressUnmanagedCodeSecurity]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseHandle(IntPtr handle);
-
-        protected override bool ReleaseHandle()
-        {
-            return CloseHandle(handle);
+            return new DataFromFileSystem(fileName, stream);
         }
     }
 }
