3714ba10bd29c4322b0d1a0e5040a9185aab1e49
Kyle Garsuta <kyle.garsuta@gmail.com>
2015-05-15 15:49:10 -0700

    Scaffold ESDAT test project

---
+++ Test/Hatfield.EnviroData.DataAcquisition.ESDAT.Test/Converters/ESDATConverterTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.DataAcquisition.ESDAT;
+
+namespace Hatfield.EnviroData.DataAcquisition.ESDAT.Test.Converters
+{
+    class ESDATConverterTest
+    {
+    }
+}
