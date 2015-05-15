7ff42ec34d03cdbd5051e5f4f8fea3683d3e1472
Kyle Garsuta <kyle.garsuta@gmail.com>
2015-05-14 17:39:49 -0700

    Modify FtpFileSystem to satisfy new specs #33 Modify FtpFileSystem class implementation to satisfy new specs Modify FtpFileSystem unit tests to verify new specs Scaffold FtpFileSystemFilter

---
+++ Source/Hatfield.EnviroData.DataAcquisition/FileSystems/Filters/FtpFileSystemFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatfield.EnviroData.DataAcquisition.Test.FileSystems.Filters
+{
+    public class FtpFileSystemFilter : IFileSystemFilter
+    {
+        public bool Meet(object value)
+        {
+            return true;
+        }
+    }
+}
