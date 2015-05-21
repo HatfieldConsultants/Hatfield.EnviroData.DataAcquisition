c4f4ab1704479881626ddc2572a9edc4403d8906
Daniel <luzhanxun.daniel@gmail.com>
2015-05-20 22:15:49 -0700

    Add ESDATDatoToImport
    Add ESDATDatoToImport

---
+++ Source/hatfield.envirodata.dataacquisition.esdat/ESDATDataToImport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.DataAcquisition.CSV;
+
+namespace Hatfield.EnviroData.DataAcquisition.ESDAT
+{
+    public class ESDATDataToImport : IDataToImport
+    {
+        private IDataToImport _headerFileToImport;
+        private CSVDataToImport _sampleFileToImport;
+        private CSVDataToImport _chemistryFileToImport;
+
+        public ESDATDataToImport(IDataToImport headerFileToImport, CSVDataToImport sampleFileToImport, CSVDataToImport chemistryFileToImport)
+        {
+            _headerFileToImport = headerFileToImport;
+            _sampleFileToImport = sampleFileToImport;
+            _chemistryFileToImport = chemistryFileToImport;
+        }
+
+        public object Data
+        {
+            get {
+                return this;
+            }
+        }
+
+        public IDataToImport HeaderFileToImport
+        {
+            get
+            {
+                return _headerFileToImport;
+            }
+        }
+
+        public CSVDataToImport SampleFileToImport
+        {
+            get { return _sampleFileToImport; }
+        }
+
+        public CSVDataToImport ChemistryFileToImport
+        {
+            get { return _chemistryFileToImport; }
+        }
+    }
+}
